d4b4b333c152ad89133f1076188ccf15bde03427
Van-Gunt Allen <vanguntallen@gmail.com>
2018-07-10 14:11:51 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="Content-Style-Type" content="text/css">
+  <title>VGA IMusic Site CMS</title>
+  <meta name="Generator" content="Cocoa HTML Writer">
+  <meta name="CocoaVersion" content="1348.17">
+  <meta name="Author" content="VanGunt Allen - @gmail.com" />
+  <style type="text/css">
+    p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; font: 36.0px Helvetica; color: #ffffff; background-color: #371a94; min-height: 43.0px}
+  </style>
+</head>
+<body>
+<p class="p1"><span class="Apple-converted-space"> IMUSIC PLUS CMS DEMO<span ><p></p>
+    
+   
+</p>   <hr>
+artist-name page<br/><br/>
+
+<?php include('./page/artist-name.html'); ?>	
+
+<hr>
+<!-- -------------------------------------------------------------
+  Function    :Body Properties Tag  
+  Description : Body Properties 
+ ----------------------------------------------------------------------> 
+ short-bio page<br/>
+<?php include('./page/short-bio.html'); ?>	
+
+<hr>
+discography page<br/>
+
+<?php include('./page/discography.html'); ?>	
+
+<hr>
+<!-- ----------------------------------------------------------------
+  Function    :Body Properties Tag  
+  Description : Body Properties 
+ ----------------------------------------------------------------------> 
+biography page
+
+<?php include('./page/biography.html'); ?>	
+
+<hr>
+events page<br/><br/>
+<?php include('./page/events.html'); ?>	
+
+<hr>
+
+<!-- -------------------------------------------------------------------
+  Function    :Body Properties Tag  
+  Description : Body Properties 
+ --------------------------------------------------------------------!> 
+ 
+ video page<br/>
+
+ <p><hr>
+  video embed page<br/>
+<?php include('./page/video.html'); ?>	
+
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     </span></p>
+</body>
+</html>
